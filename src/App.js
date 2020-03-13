67375c79e66a275c723d7a81ae7252966b50ff90
nickhayeck <nick.hayeck@gmail.com>
2020-03-12 20:59:09 -0400

    initial commit round 2

---
+++ src/App.js
@@ -0,0 +1,307 @@
+import React from 'react';
+import './App.css';
+import { motion } from "framer-motion";
+import GraphState from './backend.js';
+
+
+class Button extends React.Component{
+  render(){
+    return (<td id={this.props.isSelected ? 'selectedTool':'unselectedTool'} className='toolbarCell' onClick = {(e) => {this.props.handleClick(e)}}>
+            <img src={'icons/icons_'.concat(this.props.id,'.png')} id={this.props.id} alt='' className='buttonImage'/>
+            </td>
+           );
+  }
+}
+
+
+class FloatingToolBar extends React.Component{
+
+  render(){
+
+
+    let currentTool = this.props.selectedTool;
+    let clickHandler = this.props.clickHandler;
+
+    return (   <div className='toolbar'>
+                <table className="toolbarTable">
+                <tbody>
+                  <tr>
+                    <Button id='settings' handleClick={this.clickHandler}/>
+                    <td colSpan='6'> <p className='titleText'> Cluster State Simulator </p> </td>
+                  </tr>
+                  <tr>
+
+                    <Button id='newParticle' isSelected= {currentTool == 'newParticle'} handleClick={clickHandler}/>
+
+                    <Button id='newEdge' isSelected= {currentTool == 'newEdge'} handleClick={clickHandler}/>
+
+                    <Button id='moveParticles' isSelected= {currentTool == 'moveParticles'} handleClick={clickHandler}/>
+
+                    <Button id='unitary' isSelected= {currentTool == 'unitary'} handleClick={clickHandler}/>
+
+                    <Button id='mx' isSelected= {currentTool == 'mx'} handleClick={clickHandler}/>
+
+                    <Button id='my' isSelected= {currentTool == 'my'} handleClick={clickHandler}/>
+
+                    <Button id='mz' isSelected= {currentTool == 'mz'} handleClick={clickHandler}/>
+
+                  </tr>
+                </tbody>
+                </table>
+              </div>
+
+
+  );
+  }
+}
+
+
+class Particle extends React.Component{
+  constructor(props){
+    //Upon mounting into the DOM (always done at (0,0) position before being animated to 'initialX' and 'initialY'),
+    //create a position vector (stored in the ParticleCanvas)
+    super(props);
+    let info = this.props.info
+    info.onUpdate({x:0 , y:0},info.number)
+
+  }
+
+  render(){
+    const particleStyle = {
+      backgroundColor: 'blue',
+      borderRadius: '50%',
+      width: "10vh",
+      height: "10vh",
+      position: 'fixed',
+      top: '0vh',
+      left: '0vw',
+      border: '1vh solid #000'
+      };
+    const textStyle = {
+      textAlign: 'center',
+      verticalAlign:'middle',
+      lineHeight:'50%',
+      color:'white',
+      fontSize:'4vh',
+      pointerEvents:'none'
+    }
+    let info = this.props.info
+
+    return (<motion.div onUpdate = {(latest,id)=>info.onUpdate(latest,info.number)}
+                        animate = {{x: info.initialX, y: info.initialY}}
+                        style={particleStyle}
+                        drag={info.draggable}
+                        dragConstraints={info.particleBox}
+                        onClick = {(event,id)=>this.props.clickHandler(event,info.number)}>
+
+              <p id = 'particleText' style = {textStyle}>{info.number}</p>
+
+            </motion.div>
+          );
+  }
+}
+
+
+
+class Edge extends React.Component{
+  render(){
+    //make the code less messy
+    let v1 = this.props.firstVertex;
+    let v2 = this.props.secondVertex;
+
+    return <motion.line transition = {{duration:2}}
+                 x1={"calc("+ v1.x + "px + 6vh)"}
+                 y1={"calc("+ v1.y + "px + 6vh)"}
+                 x2={"calc("+ v2.x + "px + 6vh)"}
+                 y2={"calc("+ v2.y + "px + 6vh)"} />;
+
+  }
+}
+
+
+class ParticleCanvas extends React.Component{
+  constructor(props){
+    super(props);
+    let g = new GraphState(this.props.adjacencyMatrix)
+    this.state = {
+      positionTable : {},
+      graph : g,
+      edges : g.edges(),
+      mxhelper: [undefined, false],
+      edgehelper: [undefined,false]
+    };
+    this.particleClickHandler = this.particleClickHandler.bind(this)
+    this.onUpdate = this.onUpdate.bind(this)
+  }
+
+  onUpdate(latest,id){
+    //console.time('updating table '+id)
+    //(1) Make a copy of the positioning table, so that we can make edits and then replace the old table in the state
+    let copyTable = this.state.positionTable;
+    //(2) Slap in the new data, first making sure that there is new data to add
+    let xVal = (typeof latest.x  !== 'undefined') ? latest.x : copyTable[id]['x'];
+    let yVal = (typeof latest.y  !== 'undefined') ? latest.y : copyTable[id]['y'];
+    copyTable[id] = {x:xVal,y:yVal};
+    //(3) Replace the old table in the state
+    this.setState({positionTable : copyTable})
+
+    //console.timeEnd('updating table '+id)
+  }
+
+  particleClickHandler(event,id){
+
+    //prevent special neighbor particle selection after tool switching
+    if(this.props.selectedTool !== 'mx'){
+      this.setState({mxhelper:[undefined, false]})
+    }
+    //prevent edge creation on second particle selection after tool switching
+    if(this.props.selectedTool !== 'newEdge'){
+      this.setState({edgehelper:[undefined, false]})
+    }
+
+    //Z-axis measurement
+    if (this.props.selectedTool === 'mz'){
+      console.log('measurement in z direction initiated by click on particle #'+id);
+      let g = this.state.graph;
+      g.measureZ(id);
+      this.setState({graph:g, edges:g.edges()});
+
+    }
+
+    //Y-axis measurement
+    if (this.props.selectedTool === 'my'){
+      console.log('measurement in y direction initiated by click on particle #'+id);
+      let g = this.state.graph;
+      g.measureY(id);
+      this.setState({graph:g, edges:g.edges()});
+    }
+
+    //X-axis measurement
+    if (this.props.selectedTool === 'mx' && !this.state.mxhelper[1]){
+      console.log('measurement in x direction initiated by click on particle #'+id+" measurement will begin once a second particle is selected");
+      this.setState({mxhelper:[id,true]});
+    }
+    else if (this.props.selectedTool === 'mx' && this.state.mxhelper[1]){
+      console.log('measurement in x direction initiated with special neighbor #'+id);
+      let g = this.state.graph;
+      g.measureX(this.state.mxhelper[0],id);
+      this.setState({graph:g, edges:g.edges()});
+      this.setState({mxhelper:[undefined,false]});
+    }
+
+    //Edge Creation
+    if (this.props.selectedTool === 'newEdge' && !this.state.edgehelper[1]){
+      console.log('edge creation initiated by click on particle #'+id+" creation will begin once a second particle is selected");
+      this.setState({edgehelper:[id,true]});
+    }
+    else if (this.props.selectedTool === 'newEdge' && this.state.edgehelper[1]){
+      console.log('edge creation initiated with new neighbor #'+id);
+      let g = this.state.graph;
+      g.newEdge(this.state.edgehelper[0],id);
+      this.setState({graph:g, edges:g.edges()});
+      this.setState({edgehelper:[undefined,false]});
+    }
+  }
+
+  render(){
+
+    console.time('Render Loop Completed')
+    let particlesToRender = [];
+    let edgesToRender = [];
+    let max = this.state.graph.getAdjacencyMatrix().length;
+
+    for (let i = 0; i < max; i++){
+      let column = i*150+300;
+      let row = Math.floor(i/5)*150 + 300;
+      let particleInfo = {
+        number : i,
+        initialX : column,
+        initialY : row+Math.cos(2*i/Math.PI)*150,
+        particleBox : this.props.container,
+        draggable : this.props.canDrag,
+        onUpdate : this.onUpdate
+      };
+      particlesToRender.push(<Particle info = {particleInfo} key = {particleInfo.number.toString()} clickHandler = {this.particleClickHandler}/>);
+    }
+    //console.log(this.state.edges)
+    for(let node of this.state.edges){
+      //console.log((this.state.positionTable[node[0]] !== undefined) ? this.state.positionTable[node[0]].x : "aaabbbccc")
+      let table = this.state.positionTable;
+      if (typeof table !== 'undefined' && typeof table[node[0]] !== 'undefined' && typeof table[node[1]] !== 'undefined'){
+        edgesToRender.push(<Edge firstVertex = {table[node[0]]} secondVertex = {table[node[1]]} key = {'edge ('+node[0]+','+node[1]+')'}/>)
+      }
+    }
+
+    console.timeEnd("Render Loop Completed")
+    return(
+      <div onClick={this.newParticle}>
+
+        {particlesToRender}
+        <svg>
+          {edgesToRender}
+        </svg>
+      </div>
+    );
+
+  }
+}
+
+
+
+
+
+
+
+///////////////////////////////////////////////
+////    Begin the basic  app rendering     ////
+//////////////////////////////////////////////
+
+
+
+
+
+
+
+class Page extends React.Component{
+  constructor(props){
+    super(props);
+    this.page = React.createRef();
+    this.state = {selectedTool    : 'newParticle',
+                  adjacencyMatrix : [[0,1,1,0,1],
+                                     [1,0,1,0,0],
+                                     [1,1,0,1,0],
+                                     [0,0,1,0,1],
+                                     [1,0,0,1,0]]}
+    this.toolbarClickHandler = this.toolbarClickHandler.bind(this);
+  }
+
+  toolbarClickHandler(event){
+    this.setState({selectedTool : event.target.getAttribute("id")}, () => console.log("tool switched to: "+this.state.selectedTool));
+  }
+
+  render(){
+
+    return (
+      <div id = "page" ref = {this.page}>
+        <FloatingToolBar container = {this.page}
+                         selectedTool = {this.state.selectedTool}
+                         clickHandler = {this.toolbarClickHandler}/>
+
+
+        <ParticleCanvas container = {this.page}
+                        selectedTool = {this.state.selectedTool}
+                        canDrag = {this.state.selectedTool == 'moveParticles'}
+                        adjacencyMatrix = {this.state.adjacencyMatrix}/>
+      </div>
+    );
+  }
+}
+
+
+function App() {
+  return (
+    <Page/>
+  );
+}
+
+export default App;
---
+++ src/backend.js
@@ -0,0 +1,254 @@
+var math = require('mathjs');
+
+
+export default class GraphState{
+
+
+  neighbors(a){
+    var out = []
+    for(let i = 0; i < this.numVerticies; i++){
+      if (this.adjacencyMatrix.valueOf()[a][i] == 1){
+        out = out.concat(i);
+      }
+    }
+    return out;
+  }
+
+  edges(){
+    var out = []
+    for(let i = 0; i < this.numVerticies; i++){
+      for (let j = i; j < this.numVerticies; j++){
+        if (math.subset(this.adjacencyMatrix,math.index(i,j)) == 1){
+          out=out.concat( [[i,j]] );
+        }
+      }
+    }
+    return out
+  }
+
+  oneQubitUnitary(matrix, a){
+    var gate = (a == 0) ? matrix : [[1,0],[0,1]]
+
+    //Prepares the tensor product of the unitary with a series of identity matricies such that (if applying to the k-th qubit of n qubits)
+    //the state is prepared as I₁ ⊗ I₂ ⊗ ... ⊗ Uₖ ⊗ Iₖ₊₁ ⊗ Iₖ₊₂ ⊗ ...
+
+    for (let i = 1; i < this.numVerticies; i++){
+      if(i == a){
+        gate = math.kron(gate,matrix)
+      }else{
+        gate = math.kron(gate,[[1,0],[0,1]])
+      }
+    }
+    //apply gate to state
+    this.state = math.multiply(gate,this.state);
+  }
+
+  cZ(a, b){
+    // TODO: clean up code and add comments/equations
+    //Controlled Z on qubits a and b with a as the control
+    let identityMatrix = math.matrix([[1,0],[0,1]]);
+    let p0 = math.matrix([[1,0],[0,0]]);
+    let p1 = math.matrix([[0,0],[0,1]]);
+    let pauliZ = math.matrix([[1,0],[0,-1]])
+
+    var gateP1 = (a == 0 ? p0 : identityMatrix);
+    for (let n = 1; n < this.numVerticies; n++){
+      gateP1 = (a == n ? math.kron(gateP1, p0) : math.kron(gateP1,identityMatrix));
+    }
+
+    let gateP2 = (a == 0 ? p1 : identityMatrix);
+
+    for (let n = 1; n < this.numVerticies; n++){
+      if(a == n){
+      gateP2 = math.kron(gateP2, p1);
+      } else if(b == n) {
+      gateP2 = math.kron(gateP2, pauliZ);
+      } else {
+      gateP2 = math.kron(gateP2, identityMatrix);
+      }
+    }
+    let gate = math.add(gateP1,gateP2);
+    this.state = math.multiply(gate, this.state);
+    console.log("completed cZ on edge ("+a+","+b+")")
+  }
+
+  measureZ(a){
+    /////////////////////////////////////////////////////////
+    // Feed-forward stuff. Handle later
+    ////////////////////////////////////////////////////////
+
+    // // Measures the a-th qubit with respect to the Z-Axis
+    // //
+    // function weightedRandom(prob) {
+    //   let i, sum=0, r=Math.random();
+    //   for (i in prob) {
+    //     sum += prob[i];
+    //     if (r <= sum) return i;
+    //   }
+    // }
+    //
+    // var probabilityZero = math.complex(0,0);
+    // var probabilityOne = math.complex(0,0);
+    //
+    // for (let i = 0; i < this.state.size()){
+    //   let index = i.toString(2);
+    //   if(parseInt(index[a],10) == 1 ){
+    //     probabilityOne = math.add(probabilityOne,this.state.subset(math.index(i,0)));
+    //   }
+    //   if( parseInt(index[a],10) == 0 ){
+    //     probabilityZero = math.add(probabilityZero,this.state.subset(math.index(i,0)));
+    //   }
+    // }
+    // probabilityOne = math.multiply(probabilityOne.conjugate(),probabilityOne).re;
+    // probabilityZero = math.multiply(probabilityZero.conjugate(),probabilityZero).re;
+    //
+    // let choice = weightedRandom({0:probabilityZero , 1:probabilityOne});
+    // let aState = (choice == 1) ? math.matrix([[0],[1]]) : math.matrix([[1],[0]]);
+    let adj = this.adjacencyMatrix.valueOf()
+    for(let n of this.neighbors(a)){
+      adj[a][n] = 0;
+      adj[n][a] = 0;
+    }
+    this.adjacencyMatrix = math.matrix(adj)
+
+
+  }
+
+  measureY(a){
+    let adj = this.adjacencyMatrix.valueOf()
+    function bigE(aSet,bSet){
+      let out = []
+      for(let a of aSet){ for(let b of bSet){ if (a != b){ out = out.concat([[a,b]]); }}}
+      return out;
+    }
+
+    for(let pair of bigE(this.neighbors(a),this.neighbors(a))){
+      adj[pair[0]][pair[1]] = 0;
+    }
+    this.adjacencyMatrix = math.matrix(adj)
+
+  }
+
+  measureX(a,b0){
+    function bigE(aSet,bSet){
+      let out = []
+      for(let a of aSet){ for(let b of bSet){ if (a != b){ out = out.concat([[a,b]]); }}}
+      return out;
+    }
+    function isIn (set,element){
+      //checks if an array (called element) is contained in an array of arrays (called set)
+      for(let i of set){
+        if(i.length == element.length){
+          for(let j = 0; j < i.length; j++){
+            if (i[j] != element[j]) break;
+            if (j == i.length-1 && i[j] == element[j]) return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    let delta = (arrA,arrB)  => {
+                                  return arrA.filter(x => !isIn(arrB,x))
+                                      .concat(arrB.filter(y  => !isIn(arrA,y)));
+                                }
+    function innerArrayReverser(x){
+      let out = []
+      for(let i=0; i<x.length; i++){
+        out = out.concat([x[i]]);
+        out = out.concat([[ x[i][1] , x[i][0] ]]);
+      }
+      return out;
+    }
+
+    //////////////////////////////
+    ///End of Helper Functions///
+    /////////////////////////////
+
+
+    let intersectb0a = this.neighbors(a).filter(x => this.neighbors(b0).includes(x)) // intersection of the neighbors of a and b0
+    let up = [... new Set([...this.neighbors(a),...this.neighbors(b0)])] //the union of neighbors of a and b0, to make the bigE calculation smoother
+    let totalEdge = innerArrayReverser(this.edges()) //edges and their reverses
+
+    var finalEdges = delta( delta( delta(totalEdge , bigE(up,up)) , bigE(intersectb0a,intersectb0a)) , bigE(this.neighbors(a),this.neighbors(a)))
+    console.log('finalEdges',finalEdges)
+
+    let adj = this.adjacencyMatrix.valueOf()
+
+//if the matrix element is not in the new edges list, set it to zero, otherwise, set it to one
+    for (let i=0; i<adj.length;i++){
+      for(let j=0; j<=i;j++){
+        if(!isIn(finalEdges,[i,j])){
+          adj[i][j] = 0;
+          adj[j][i] = 0;
+        } else {
+          adj[i][j] = 1;
+          adj[j][i] = 1;
+        }
+      }
+    }
+    this.adjacencyMatrix = math.matrix(adj)
+
+  }
+
+  newEdge(a,b){
+
+
+    let adj = this.adjacencyMatrix.valueOf()
+    if(adj[a][b] === 1 || adj[b][a] === 1) console.warn('There already exists an edge there! ('+a+','+b+') is already in the list of edges');
+    adj[a][b] = 1;
+    adj[b][a] = 1;
+    this.adjacencyMatrix = math.matrix(adj)
+  }
+
+  getAdjacencyMatrix(){
+    return this.adjacencyMatrix.valueOf()
+  }
+
+  constructor(inpAdjacencyMatrix){
+    //check the matrix for size & symmetry
+    let size = math.matrix(inpAdjacencyMatrix).size()
+    if(size[0] != size[1]){
+      throw new Error("Mishapen Adjacency Matrix :: The given matrix is "+size[0]+"x"+size[1]+", but it should be square (e.g. "+size[0]+"x"+size[0]+")")
+    }
+    this.numVerticies = size[0];
+    for(let i = 0; i < this.numVerticies; i++){
+      for (let j = i; j < this.numVerticies; j++){
+        if (inpAdjacencyMatrix[i][j] != inpAdjacencyMatrix[j][i]){
+          throw new Error("Non-symmetric Adjacency Matrix :: The given value at "+i+","+j+" should be equal to the value at "+j+","+i);
+        }
+      }
+    }
+
+    //apply constants
+    this.adjacencyMatrix = math.matrix(inpAdjacencyMatrix);
+    const singlestate = math.matrix([[1/math.sqrt(2)],[1/math.sqrt(2)]]);
+
+    //construct blank graph
+    this.state = singlestate;
+    for (let n = 0; n < this.numVerticies-1; n++){
+      this.state = math.kron(this.state,singlestate)
+    }
+
+    for(let edge of this.edges()){
+      console.log("performing cZ on ("+edge[0]+","+edge[1]+")")
+      this.cZ(edge[0], edge[1]);
+    }
+    console.log(":: Blank Graph Construction Complete ::");console.log()
+  }
+
+  getState(indexFormat='bin'){
+    var out = ''
+    for(let i = 0; i < math.pow(2,this.numVerticies); i++){
+      if (indexFormat == 'bin'){
+        var index = i.toString(2).padStart(this.numVerticies,'0');
+      }else{
+        var index = i
+      }
+      out = out.concat('|'+index+'>\t' + this.state.subset(math.index(i,0))+'\n')
+    }
+    return out
+  }
+
+
+}
