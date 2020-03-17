b5d6bf90ebb9be3aeb34b85ba7cf4f2eec243314
nickhayeck <nick.hayeck@gmail.com>
2020-03-17 13:06:16 -0400

    Minor update again to fix build structure

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Cluster State Simulator/p);
+  const linkElement = getByText(/Cluster State Simulator/i);
   expect(linkElement).toBeInTheDocument();
 });
