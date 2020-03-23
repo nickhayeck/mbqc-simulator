ad50b9ee5ee2aeb3fec2a1e22cee9a2103560fa7
nickhayeck <nick.hayeck@gmail.com>
2020-03-23 16:46:21 -0400

    Updates

---
+++ precache-manifest.e2f3fe5ce2b79015602ee3e5ffa8ad2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eac91ec4d7435c73919772406ee0d46",
+    "url": "/mbqc-simulator/index.html"
+  },
+  {
+    "revision": "c68341e44cff29893ae8",
+    "url": "/mbqc-simulator/static/css/main.fcab3dbe.chunk.css"
+  },
+  {
+    "revision": "39a6335c66f886e7abcb",
+    "url": "/mbqc-simulator/static/js/2.e1a54196.chunk.js"
+  },
+  {
+    "revision": "dcf9e279c96709921dd1f85dfeaf444e",
+    "url": "/mbqc-simulator/static/js/2.e1a54196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68341e44cff29893ae8",
+    "url": "/mbqc-simulator/static/js/main.95819f92.chunk.js"
+  },
+  {
+    "revision": "9ab7273f9ed90c866912",
+    "url": "/mbqc-simulator/static/js/runtime-main.89913920.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mbqc-simulator/precache-manifest.2a96544a878a0d093bd4d6f4d4a715f7.js"
+  "/mbqc-simulator/precache-manifest.e2f3fe5ce2b79015602ee3e5ffa8ad2e.js"
 );
 
 self.addEventListener('message', (event) => {
